ee1cc6946542e1d4ef3937a310f15136b3128015
bryn austin bellomy <bryn.bellomy@gmail.com>
2017-08-19 21:39:00 -0500

    Clean up tests a little

--- test/remittance.js
+++ test/remittance.js
@@ -26,89 +26,72 @@ contract('Remittance', accounts => {
         await expectThrow( remittance.deposit('abcdefg', blockNumber, { from: accounts[0], value: 100 }) )
     })
 
-    it('should deposit correctly', async () => {
-        let pw1 = web3.sha3('xyzzy')
-        let pw2 = web3.sha3('foobar')
-        let password = web3.sha3(pw1.substr(2) + pw2.substr(2), { encoding: 'hex' })
-        let blockNumber = await web3.eth.getBlockNumberPromise()
-        let depositAmount = web3.toWei(1, 'ether')
+    describe('when something is deposited', async () => {
+        let pw1, pw2, password, blockNumber, depositAmount, tx
 
-        let tx = await remittance.deposit(password, blockNumber + 100, { from: accounts[0], value: depositAmount })
+        beforeEach(async () => {
+            pw1 = web3.sha3('xyzzy')
+            pw2 = web3.sha3('foobar')
+            password = web3.sha3(pw1.substr(2) + pw2.substr(2), { encoding: 'hex' })
+            blockNumber = await web3.eth.getBlockNumberPromise()
+            depositAmount = web3.toWei(1, 'ether')
 
-        assert.equal(tx.logs.length, 1, 'Should have gotten 1 event in the log')
-        assert.equal(tx.logs[0].event, 'LogReceived', 'Should have gotten a LogReceived event')
-        assert.equal(tx.logs[0].args.pwHash, password, `LogReceived event should have the password hash ${password}`)
-        assert.equal(tx.logs[0].args.value, web3.toWei(1, 'ether'), 'LogReceived event should have the value 1 ETH')
+            tx = await remittance.deposit(password, blockNumber + 100, { from: accounts[0], value: depositAmount })
+        })
 
-        let [ depositor, amount, deadlineBlock ] = await remittance.entriesByHash(password)
-        assert.equal(depositor, accounts[0], `Depositor should be ${accounts[0]}, got ${depositor}`)
-        assert.equal(amount, depositAmount, `Amount should be ${depositAmount}, got ${amount}.`)
-        assert.equal(deadlineBlock, blockNumber + 100, `Deadline block should be ${blockNumber + 100}, got ${deadlineBlock}`)
-    })
+        it('should log the deposit', async () => {
+            assert.equal(tx.logs.length, 1, 'Should have gotten 1 event in the log')
+            assert.equal(tx.logs[0].event, 'LogReceived', 'Should have gotten a LogReceived event')
+            assert.equal(tx.logs[0].args.pwHash, password, `LogReceived event should have the password hash ${password}`)
+            assert.equal(tx.logs[0].args.value, web3.toWei(1, 'ether'), 'LogReceived event should have the value 1 ETH')
+        })
 
-    it('should prevent multiple deposits under the same password hash', async () => {
-        let pw1 = web3.sha3('xyzzy')
-        let pw2 = web3.sha3('foobar')
-        let password = web3.sha3(pw1.substr(2) + pw2.substr(2), { encoding: 'hex' })
-        let blockNumber = await web3.eth.getBlockNumberPromise()
+        it('should reflect the deposit in entriesByHash', async () => {
+            let [ depositor, amount, deadlineBlock ] = await remittance.entriesByHash(password)
+            assert.equal(depositor, accounts[0], `Depositor should be ${accounts[0]}, got ${depositor}`)
+            assert.equal(amount, depositAmount, `Amount should be ${depositAmount}, got ${amount}.`)
+            assert.equal(deadlineBlock, blockNumber + 100, `Deadline block should be ${blockNumber + 100}, got ${deadlineBlock}`)
+        })
 
-        await remittance.deposit(password, blockNumber + 100, { from: accounts[0], value: web3.toWei(1, 'ether') })
-        await expectThrow(
-            remittance.deposit(password, blockNumber + 100, { from: accounts[1], value: web3.toWei(1, 'ether') })
-        )
-    })
+        it('should prevent multiple deposits under the same password hash', async () => {
+            await expectThrow(
+                remittance.deposit(password, blockNumber + 100, { from: accounts[1], value: web3.toWei(1, 'ether') })
+            )
+        })
 
-    it('should prevent multiple deposits under the same password hash, even after funds are withdrawn fully', async () => {
-        let pw1 = web3.sha3('xyzzy')
-        let pw2 = web3.sha3('foobar')
-        let password = web3.sha3(pw1.substr(2) + pw2.substr(2), { encoding: 'hex' })
-        let blockNumber = await web3.eth.getBlockNumberPromise()
+        it('should prevent multiple deposits under the same password hash, even after funds are withdrawn fully', async () => {
+            await remittance.withdraw(pw1, pw2, { from: accounts[1] })
+            await expectThrow(
+                remittance.deposit(password, blockNumber + 100, { from: accounts[1], value: web3.toWei(1, 'ether') })
+            )
+        })
 
-        await remittance.deposit(password, blockNumber + 100, { from: accounts[0], value: web3.toWei(1, 'ether') })
-        await remittance.withdraw(pw1, pw2, { from: accounts[1] })
-        await expectThrow(
-            remittance.deposit(password, blockNumber + 100, { from: accounts[1], value: web3.toWei(1, 'ether') })
-        )
-    })
+        it('should allow withdrawals', async () => {
+            let prevBalance = await web3.eth.getBalancePromise(accounts[1])
 
-    it('should allow withdrawals', async () => {
-        let pw1 = web3.sha3('xyzzy')
-        let pw2 = web3.sha3('foobar')
-        let password = web3.sha3(pw1.substr(2) + pw2.substr(2), { encoding: 'hex' })
-        let blockNumber = await web3.eth.getBlockNumberPromise()
+            let tx = await remittance.withdraw(pw1, pw2, { from: accounts[1] })
+            let gasPrice = (await web3.eth.getTransactionPromise(tx.tx)).gasPrice
+            let totalGasCost = gasPrice.times(tx.receipt.gasUsed)
 
-        await remittance.deposit(password, blockNumber + 100, { from: accounts[0], value: web3.toWei(1, 'ether') })
+            let newBalance = await web3.eth.getBalancePromise(accounts[1])
 
-        let prevBalance = web3.eth.getBalance(accounts[1])
+            assert(tx.logs.length === 1, `Expected 1 log, got ${tx.logs.length}`)
+            let log = tx.logs[0]
 
-        let tx = await remittance.withdraw(pw1, pw2, { from: accounts[1] })
-        let gasPrice = (await web3.eth.getTransactionPromise(tx.tx)).gasPrice
-        let totalGasCost = gasPrice.times(tx.receipt.gasUsed)
+            assert(log.event === 'LogWithdrawal', `Expected LogWithdrawal, got ${log.event}`)
+            assert(log.args.to === accounts[1], `LogWithdrawal.args.to should have been ${accounts[1]}, got ${log.args.to}`)
+            assert(log.args.value.equals(web3.toWei(1, 'ether')), `LogWithdrawal.args.value should have been ${web3.toWei(1, 'ether')}, got ${log.args.value}`)
 
-        let newBalance = web3.eth.getBalance(accounts[1])
+            let oneETH = web3.toBigNumber(web3.toWei(1, 'ether'))
+            assert(newBalance.minus(prevBalance).equals( oneETH.minus(totalGasCost) ), `Balance is wrong after withdrawal (${newBalance.minus(prevBalance).toString()})`)
+        })
 
-        assert(tx.logs.length === 1, `Expected 1 log, got ${tx.logs.length}`)
-        let log = tx.logs[0]
-
-        assert(log.event === 'LogWithdrawal', `Expected LogWithdrawal, got ${log.event}`)
-        assert(log.args.to === accounts[1], `LogWithdrawal.args.to should have been ${accounts[1]}, got ${log.args.to}`)
-        assert(log.args.value.equals(web3.toWei(1, 'ether')), `LogWithdrawal.args.value should have been ${web3.toWei(1, 'ether')}, got ${log.args.value}`)
-
-        let oneETH = web3.toBigNumber(web3.toWei(1, 'ether'))
-        assert(newBalance.minus(prevBalance).equals( oneETH.minus(totalGasCost) ), `Balance is wrong after withdrawal (${newBalance.minus(prevBalance).toString()})`)
-    })
-
-    it('should prevent withdrawing more than once', async () => {
-        let pw1 = web3.sha3('xyzzy')
-        let pw2 = web3.sha3('foobar')
-        let password = web3.sha3(pw1.substr(2) + pw2.substr(2), { encoding: 'hex' })
-        let blockNumber = await web3.eth.getBlockNumberPromise()
-
-        await remittance.deposit(password, blockNumber + 100, { from: accounts[0], value: web3.toWei(1, 'ether') })
-        await remittance.withdraw(pw1, pw2, { from: accounts[1] })
-        await expectThrow(
-            remittance.withdraw(pw1, pw2, { from: accounts[1] })
-        )
+        it('should prevent withdrawing more than once', async () => {
+            await remittance.withdraw(pw1, pw2, { from: accounts[1] })
+            await expectThrow(
+                remittance.withdraw(pw1, pw2, { from: accounts[1] })
+            )
+        })
     })
 })
 
